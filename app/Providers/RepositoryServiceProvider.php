660766f0ffc7d01fc89b55efc03ec702e4480674
Aminah Nuraini <aminah.nuraini92@gmail.com>
2023-08-10 12:35:52 +0700

    - Remove Redis and go back to Eloquent - Raise scrapers frequency

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Repositories\Redis\JobRepository;
+// use App\Repositories\Redis\JobRepository;
+use App\Repositories\Eloquent\JobRepository;
 use App\Repositories\JobRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
